e33e5f86b95e1bcfdf39d09ab2e34b028a8b559a
Kyle McLamb <kjmclamb@gmail.com>
2019-10-14 00:33:08 -0400

    Add N-body tests from the benchmarks game

--- js/duktape-benchmarks.cpp
+++ js/duktape-benchmarks.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+/* !!! REMINDER: stack starts at 0 !!! */
 
 #include "duktape.h"
 
@@ -9,3 +10,61 @@ static void BM_CreateVM(benchmark::State& state) {
 	}
 }
 BENCHMARK(BM_CreateVM);
+
+namespace {
+int32_t loadfile(duk_context* ctx, const char* filename) {
+    FILE *f;
+    size_t len;
+    char buf[16384];
+
+    f = fopen(filename, "rb");
+    if (!f) {
+        duk_push_undefined(ctx);
+		return 1; // error
+	}
+
+	len = fread((void *) buf, 1, sizeof(buf), f);
+	fclose(f);
+
+	int32_t dukflags = 0;
+	duk_push_string(ctx, filename);
+	return duk_pcompile_lstring_filename(ctx, dukflags, (const char *) buf, (duk_size_t) len);
+}
+}
+
+static void BM_LoadNBody(benchmark::State& state) {
+	duk_context* ctx = duk_create_heap_default();
+    for (auto _ : state) {
+		if (loadfile(ctx, "nbody.js") != 0) {
+			state.SkipWithError(duk_safe_to_string(ctx, -1));
+			duk_pop(ctx);
+			break;
+		}
+		duk_pop(ctx);
+	}
+	duk_destroy_heap(ctx);
+}
+BENCHMARK(BM_LoadNBody);
+
+static void BM_RunNBody(benchmark::State& state) {
+	duk_context* ctx = duk_create_heap_default();
+	if (loadfile(ctx, "nbody.js") != 0) { // [0]
+		state.SkipWithError(duk_safe_to_string(ctx, -1));
+		duk_pop(ctx);
+	}
+	else
+	{
+		for (auto _ : state) {
+			duk_dup(ctx, 0); // [1] = nbody.js
+			duk_push_int(ctx, state.range(0)); // [2]
+			if(duk_pcall(ctx, 1) != DUK_EXEC_SUCCESS) {
+				state.SkipWithError(duk_safe_to_string(ctx, -1));
+				duk_pop(ctx); // [1]
+				break;
+			}
+			duk_pop(ctx); // [1]
+		}
+	}
+	duk_destroy_heap(ctx);
+}
+BENCHMARK(BM_RunNBody)->Arg(1)->Arg(10)->Arg(100)->Unit(benchmark::kMillisecond);
--- lua/benchmarks.cpp
+++ lua/benchmarks.cpp
@@ -1,6 +1,10 @@
 #include <benchmark/benchmark.h>
 
-#include "lua.hpp"
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
 
 static void BM_CreateVM(benchmark::State& state) {
     for (auto _ : state) {
@@ -10,3 +14,38 @@ static void BM_CreateVM(benchmark::State& state) {
 	}
 }
 BENCHMARK(BM_CreateVM);
+
+static void BM_LoadNBody(benchmark::State& state) {
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+    for (auto _ : state) {
+		luaL_loadfile(L, "nbody.lua"); // [1]
+		if (!lua_isfunction(L, 1)) {
+			state.SkipWithError(lua_tostring(L, 1));
+			break;
+		}
+	}
+	lua_close(L);
+}
+BENCHMARK(BM_LoadNBody);
+
+static void BM_RunNBody(benchmark::State& state) {
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	luaL_loadfile(L, "nbody.lua"); // [1]
+	if (!lua_isfunction(L, 1)) {
+		state.SkipWithError(lua_tostring(L, 1));
+		lua_close(L);
+		return;
+	}
+    for (auto _ : state) {
+			lua_pushvalue(L, 1); // [2]
+			lua_pushinteger(L, state.range(0)); // [3]
+			if (lua_pcall(L, 1, 0, 0) != 0) { // [2]([3]) -> [2]
+				state.SkipWithError(lua_tostring(L, -1));
+				break;
+			}
+	}
+	lua_close(L);
+}
+BENCHMARK(BM_RunNBody)->Arg(1)->Arg(10)->Arg(100)->Unit(benchmark::kMillisecond);
